7c258ab3be54851fdca70381739d0b433b4c27a6
ChakshuGautam <chaks.gautam@gmail.com>
2022-05-23 22:58:27 +0530

    Fix: env file not present

--- gitpod.install.sh
+++ gitpod.install.sh
@@ -11,15 +11,15 @@ nvm use 16
 
 # Start DB and services 
 cp src/sample.env .env
-mkdir broker
-sudo chown -R 1001:1001 broker # Bitnami RabbitMQ issues
+cp .env src/.env
+mkdir -p broker
+sudo chown -R 1001:1001 broker
 docker-compose up -d shortdb gql yaus-broker shortnr-cache shortnr-redis-commander
 
 echo "the script will sleep for 2 mins to let the services start"
 sleep 2m
 
 # Starting Server
-cp .env src/.env
 cd src
 serverBaseURl="SERVER_BASE_URL=${GITPOD_WORKSPACE_URL:-default_value}"
 sed "13s|^.*$|$serverBaseURl|" .env > .env.tmp
