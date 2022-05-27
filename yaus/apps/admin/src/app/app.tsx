8177a2446abec99df0c68f684b79ed49323ff4df
tushar5526 <codingid6@gmail.com>
2022-05-28 03:08:39 +0530

    Added a basic setup of react-admin

--- yaus/apps/admin/src/app/app.tsx
+++ yaus/apps/admin/src/app/app.tsx
@@ -1,14 +1,52 @@
-import * as React from "react";
-import { Admin, Resource } from "react-admin";
-import jsonServerProvider from "ra-data-json-server";
-import { PostList } from "./posts";
+import * as React from 'react';
+import { Admin, Resource, DataProvider, ListGuesser } from 'react-admin';
+import buildHasuraProvider from 'ra-data-hasura';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { useState, useEffect } from 'react';
+import Dashboard from './components/Dashboard';
 
-const App = () => (
-  <Admin
-    dataProvider={jsonServerProvider("https://jsonplaceholder.typicode.com")}
-  >
-    <Resource name="posts" list={PostList} options={{}} />
-  </Admin>
-);
+import { createBrowserHistory as createHistory } from 'history';
+const history = createHistory();
+
+const createApolloClient = async () => {
+  console.log(process.env)
+  return new ApolloClient({
+    // uri: 'http://localhost:15003/v1/graphql',
+    uri: `${process.env.NX_HASURA_URL}`,
+    cache: new InMemoryCache(),
+    headers: {
+      "x-hasura-admin-secret": `${process.env.NX_HASURA_GRAPHQL_ADMIN_SECRET}`,
+      // "x-hasura-admin-secret": 'adminSuperSecret',
+    },
+  });
+};
+const App = () => { 
+  const [dataProvider, setDataProvider] = useState({});
+
+  useEffect(() => {
+      const buildDataProvider = async () => {
+
+          const apolloClient = await createApolloClient();
+
+          const hasuraDataProvider = await buildHasuraProvider({
+              client: apolloClient
+          });
+          setDataProvider(() => hasuraDataProvider);
+      }
+      buildDataProvider();
+  }, []);
+
+
+  return (
+      <Admin 
+          dataProvider={dataProvider as DataProvider} 
+          title="Hasura Dashboard"
+          dashboard={Dashboard}
+          history={history}
+      >
+      <Resource name="link" list={ListGuesser} />
+      </Admin>
+  )
+};
 
 export default App;
