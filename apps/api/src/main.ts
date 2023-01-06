c59aa117563995e4edac85a65667f9a34b519fd6
Abhishek Mishra <amonabhi143@gmail.com>
2023-01-06 17:26:40 +0530

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -48,7 +48,7 @@ async function bootstrap() {
 
   const port = process.env.PORT || 3333;
   await app.startAllMicroservices();
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
