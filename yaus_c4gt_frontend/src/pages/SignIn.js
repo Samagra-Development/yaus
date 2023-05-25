b0702c3ac5bc997f4a75e0142614dda2c4d641ae
Rishabh Jain <jain.rishabhche@gmail.com>
2023-05-25 20:50:43 +0530

    Refactor: SVG icons and routes

--- yaus_c4gt_frontend/src/pages/SignIn.js
+++ yaus_c4gt_frontend/src/pages/SignIn.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useHistory } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
+import Routes from "../assets/constants/routes.js";
 import {
   Layout,
   Menu,
@@ -12,6 +12,11 @@ import {
   Input,
   Switch,
 } from "antd";
+import { ReactComponent as DashboardIcon } from "../assets/icons/dashboardIcon.svg";
+import { ReactComponent as ProfileIcon } from "../assets/icons/profileIcon.svg";
+import { ReactComponent as SigninIcon } from "../assets/icons/signinIcon.svg";
+import { ReactComponent as SignupIcon } from "../assets/icons/signupIcon.svg";
+
 import signinbg from "../assets/images/img-signin.jpg";
 import {
   DribbbleOutlined,
@@ -19,91 +24,12 @@ import {
   InstagramOutlined,
   GithubOutlined,
 } from "@ant-design/icons";
+
 function onChange(checked) {
   console.log(`switch to ${checked}`);
 }
 const { Title } = Typography;
 const { Header, Footer, Content } = Layout;
-const template = [
-  <svg
-    data-v-4ebdc598=""
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      data-v-4ebdc598=""
-      d="M3 4C3 3.44772 3.44772 3 4 3H16C16.5523 3 17 3.44772 17 4V6C17 6.55228 16.5523 7 16 7H4C3.44772 7 3 6.55228 3 6V4Z"
-      fill="#111827"
-      className="fill-muted"
-    ></path>
-    <path
-      data-v-4ebdc598=""
-      d="M3 10C3 9.44771 3.44772 9 4 9H10C10.5523 9 11 9.44771 11 10V16C11 16.5523 10.5523 17 10 17H4C3.44772 17 3 16.5523 3 16V10Z"
-      fill="#111827"
-      className="fill-muted"
-    ></path>
-    <path
-      data-v-4ebdc598=""
-      d="M14 9C13.4477 9 13 9.44771 13 10V16C13 16.5523 13.4477 17 14 17H16C16.5523 17 17 16.5523 17 16V10C17 9.44771 16.5523 9 16 9H14Z"
-      fill="#111827"
-      className="fill-muted"
-    ></path>
-  </svg>,
-];
-const profile = [
-  <svg
-    data-v-4ebdc598=""
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      data-v-4ebdc598=""
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M18 10C18 14.4183 14.4183 18 10 18C5.58172 18 2 14.4183 2 10C2 5.58172 5.58172 2 10 2C14.4183 2 18 5.58172 18 10ZM12 7C12 8.10457 11.1046 9 10 9C8.89543 9 8 8.10457 8 7C8 5.89543 8.89543 5 10 5C11.1046 5 12 5.89543 12 7ZM9.99993 11C7.98239 11 6.24394 12.195 5.45374 13.9157C6.55403 15.192 8.18265 16 9.99998 16C11.8173 16 13.4459 15.1921 14.5462 13.9158C13.756 12.195 12.0175 11 9.99993 11Z"
-      fill="#111827"
-      className="fill-muted"
-    ></path>
-  </svg>,
-];
-const signup = [
-  <svg
-    data-v-4ebdc598=""
-    width="20"
-    height="20"
-    viewBox="0 0 20 20"
-    fill="none"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      data-v-4ebdc598=""
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M6 2C5.44772 2 5 2.44772 5 3V4H4C2.89543 4 2 4.89543 2 6V16C2 17.1046 2.89543 18 4 18H16C17.1046 18 18 17.1046 18 16V6C18 4.89543 17.1046 4 16 4H15V3C15 2.44772 14.5523 2 14 2C13.4477 2 13 2.44772 13 3V4H7V3C7 2.44772 6.55228 2 6 2ZM6 7C5.44772 7 5 7.44772 5 8C5 8.55228 5.44772 9 6 9H14C14.5523 9 15 8.55228 15 8C15 7.44772 14.5523 7 14 7H6Z"
-      fill="#111827"
-      className="fill-muted"
-    ></path>
-  </svg>,
-];
-const signin = [
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="14"
-    height="14"
-    viewBox="0 0 14 14"
-  >
-    <path
-      className="fill-muted"
-      d="M12.25,14H1.75A1.752,1.752,0,0,1,0,12.25V3.5A1.752,1.752,0,0,1,1.75,1.75h.876V.875a.875.875,0,0,1,1.75,0V1.75h5.25V.875a.875.875,0,0,1,1.75,0V1.75h.875A1.752,1.752,0,0,1,14,3.5v8.75A1.752,1.752,0,0,1,12.25,14ZM3.5,4.375a.875.875,0,0,0,0,1.75h7a.875.875,0,0,0,0-1.75Z"
-    />
-  </svg>,
-];
 
 function SignIn() {
   const [email, setEmail] = useState("");
@@ -129,7 +55,7 @@ function SignIn() {
     let item = {
       loginId: email,
       password: password,
-      applicationId: "650c50b5-eae9-442e-88de-fc36be10a21b",
+      applicationId: `${process.env.REACT_APP_APPLICATION_ID}`,
     };
 
     let result = await fetch("https://fa.chakshu-rd.samagra.io/api/login", {
@@ -151,40 +77,34 @@ function SignIn() {
     <>
       <Layout className="layout-default layout-signin">
         <Header>
-          {/* <div className="header-col header-brand">
-              <h5>Muse Dashboard</h5>
-            </div> */}
           <div className="header-col header-nav">
             <Menu mode="horizontal" defaultSelectedKeys={["1"]}>
               <Menu.Item key="1">
-                <Link to="/dashboard">
-                  {template}
+                <Link to={Routes.DASHBOARD}>
+                  <DashboardIcon />
                   <span> Dashboard</span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="2">
-                <Link to="/profile">
-                  {profile}
+                <Link to={Routes.PROFILE}>
+                  <ProfileIcon />
                   <span>Profile</span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="3">
-                <Link to="/sign-up">
-                  {signup}
+                <Link to={Routes.SIGN_UP}>
+                  <SignupIcon />
                   <span> Sign Up</span>
                 </Link>
               </Menu.Item>
               <Menu.Item key="4">
-                <Link to="/sign-in">
-                  {signin}
+                <Link to={Routes.SIGN_IN}>
+                  <SigninIcon />
                   <span> Sign In</span>
                 </Link>
               </Menu.Item>
             </Menu>
           </div>
-          {/* <div className="header-col header-btn">
-              <Button type="primary">FREE DOWNLOAD</Button>
-            </div> */}
         </Header>
         <Content className="signin">
           <Row gutter={[24, 0]} justify="space-around">
