20af2631700c22af6c6c3a27edef4477140e9d34
Rishabh Jain <jain.rishabhche@gmail.com>
2023-05-27 00:11:03 +0530

    Refactor: Home and Recent_activities  pages

---
+++ yaus_c4gt_frontend/src/assets/constants/mockData.js
@@ -0,0 +1,274 @@
+import dollor from "../icons/dollor.svg";
+import heart from "../icons/heart.svg";
+import cart from "../icons/cart.svg";
+import profile from "../icons/homeProfile.svg";
+
+export const colRecentActivities = [
+  {
+    title: "Created",
+    dataIndex: "date",
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: "Marketing Title",
+    dataIndex: "org",
+    filters: [
+      {
+        text: "Samagra Website",
+        value: "Samagra Website",
+      },
+      {
+        text: "My First Link",
+        value: "My First Link",
+      },
+      {
+        text: "Competency Passbook",
+        value: "Competency Passbook",
+      },
+      {
+        text: "Hackerank",
+        value: "Hackerank",
+      },
+    ],
+    filterMode: "tree",
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
+    width: "30%",
+  },
+  {
+    title: "Activities",
+    dataIndex: "url",
+    filters: [
+      {
+        text: "samagra",
+        value: "https://yaus.xyz/samagra",
+      },
+      {
+        text: "first",
+        value: "https://yaus.xyz/first",
+      },
+      {
+        text: "pass",
+        value: "https://yaus.xyz/pass",
+      },
+      {
+        text: "hack",
+        value: "https://yaus.xyz/hack",
+      },
+    ],
+    onFilter: (value, record) => record.url.startsWith(value),
+    filterSearch: true,
+    width: "40%",
+  },
+  {
+    title: "Name",
+    dataIndex: "name",
+    filters: [
+      {
+        text: "Kanika",
+        value: "kanika",
+      },
+      {
+        text: "Akshay",
+        value: "akshay",
+      },
+      {
+        text: "Chakshu",
+        value: "chakshu",
+      },
+      {
+        text: "Manav",
+        value: "manav",
+      },
+    ],
+    filterMode: "tree",
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
+    width: "30%",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    filters: [
+      {
+        text: "kanikagola26@gmail.com",
+        value: "kanikagola26@gmail.com",
+      },
+      {
+        text: "akshay@gmail.com",
+        value: "akshay@gmail.com",
+      },
+      {
+        text: "chakshu@gmail.com",
+        value: "chakshu@gmail.com",
+      },
+      {
+        text: "manav@gmail.com",
+        value: "manav@gmail.com",
+      },
+    ],
+    filterMode: "tree",
+    filterSearch: true,
+    onFilter: (value, record) => record.name.includes(value),
+    width: "30%",
+  },
+];
+export const data = [
+  {
+    date: "",
+    org: "",
+    url: <a href=""></a>,
+    name: "",
+    email: "",
+  },
+  {
+    date: "",
+    org: "",
+    url: <a href=""></a>,
+    name: "",
+    email: "",
+  },
+  {
+    date: "",
+    org: "",
+    url: <a href=""></a>,
+    name: "",
+    email: "",
+  },
+  {
+    date: "",
+    org: "",
+    url: <a href=""></a>,
+    name: "",
+    email: "",
+  },
+];
+export const piedata = [
+  {
+    type: "Week 1",
+    value: 45,
+  },
+  {
+    type: "Week 2",
+    value: 55,
+  },
+  {
+    type: "Week 3",
+    value: 47,
+  },
+  {
+    type: "Week 4",
+    value: 61,
+  },
+  {
+    type: "Week 5",
+    value: 72,
+  },
+  {
+    type: "Week 6",
+    value: 34,
+  },
+];
+
+export const count = [
+  {
+    today: "Clicks",
+    title: "335",
+    persent: "+30%",
+    icon: dollor,
+    bnb: "bnb2",
+  },
+  {
+    today: "Installs",
+    title: "-",
+    persent: "+20%",
+    icon: profile,
+    bnb: "bnb2",
+  },
+  {
+    today: "Open",
+    title: "+144",
+    persent: "20%",
+    icon: heart,
+    bnb: "redtext",
+  },
+  {
+    today: "Views",
+    title: "82",
+    persent: "10%",
+    icon: cart,
+    bnb: "bnb2",
+  },
+];
+
+export const columns = [
+  {
+    title: "Date",
+    dataIndex: "date",
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: "Short URL",
+    dataIndex: "url",
+    filters: [
+      {
+        text: "samagra",
+        value: "https://yaus.xyz/samagra",
+      },
+      {
+        text: "first",
+        value: "https://yaus.xyz/first",
+      },
+      {
+        text: "pass",
+        value: "https://yaus.xyz/pass",
+      },
+      {
+        text: "hack",
+        value: "https://yaus.xyz/hack",
+      },
+    ],
+    onFilter: (value, record) => record.url.startsWith(value),
+    filterSearch: true,
+    width: "35%",
+  },
+  {
+    title: "Views",
+    dataIndex: "views",
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: "Installs",
+    dataIndex: "install",
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: "Clicks",
+    dataIndex: "clicks",
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: "Opens",
+    dataIndex: "open",
+    sorter: (a, b) => a.age - b.age,
+  },
+  {
+    title: "Action",
+    dataIndex: "action",
+  },
+];
+
+export const myData = [
+  { x: "Jan", y: 45 },
+  { x: "Feb", y: 42 },
+  { x: "Mar", y: 53 },
+  { x: "Apr", y: 62 },
+  { x: "May", y: 64 },
+  { x: "Jun", y: 54 },
+  { x: "Jul", y: 59 },
+  { x: "Aug", y: 47 },
+  { x: "Sep", y: 0 },
+  { x: "Oct", y: 0 },
+  { x: "Nov", y: 0 },
+  { x: "Dec", y: 0 },
+];
--- yaus_c4gt_frontend/src/pages/Recent_Activities.js
+++ yaus_c4gt_frontend/src/pages/Recent_Activities.js
@@ -1,32 +1,29 @@
-import { Table, Button } from 'antd';
-import React, { useState } from 'react'
-import { AudioOutlined } from '@ant-design/icons';
-import { Input, Space } from 'antd';
+import { Table, Button } from "antd";
+import React, { useState } from "react";
+import { AudioOutlined } from "@ant-design/icons";
+import { Input, Space } from "antd";
 import { NavLink, useLocation } from "react-router-dom";
+import { colRecentActivities, data } from "../assets/constants/mockData";
 import { Excel } from "antd-table-saveas-excel";
 const { Search } = Input;
 const suffix = (
   <AudioOutlined
     style={{
       fontSize: 16,
-      color: 'white',
+      color: "white",
     }}
   />
 );
 const onSearch = (value) => console.log(value);
 
-
-const App = () =>
-(
+const App = () => (
   <>
-    <NavLink to="/LinkCreate"><Button type="primary">
-      Create Your Link
-    </Button>
+    <NavLink to="/LinkCreate">
+      <Button type="primary">Create Your Link</Button>
     </NavLink>
     <br></br>
     <br></br>
     <br></br>
-
     <Search
       placeholder="Search"
       onSearch={onSearch}
@@ -39,139 +36,17 @@ const App = () =>
     <h2>Start editing to see some magic happen!</h2>
     <br></br>
     <br></br>
-    <Table columns={columns} dataSource={data} onChange={onChange} />;
-
+    <Table
+      columns={colRecentActivities}
+      dataSource={data}
+      onChange={onChange}
+    />
+    ;
   </>
-)
-
-const columns = [
-  {
-    title: 'Created',
-    dataIndex: 'date',
-    sorter: (a, b) => a.age - b.age,
-  },
-  {
-    title: 'Marketing Title',
-    dataIndex: 'org',
-    filters: [
-      {
-        text: 'Samagra Website',
-        value: 'Samagra Website',
-      },
-      {
-        text: 'My First Link',
-        value: 'My First Link',
-
-      },
-      {
-        text: 'Competency Passbook',
-        value: 'Competency Passbook',
-
-      },
-      {
-        text: 'Hackerank',
-        value: 'Hackerank',
-
-      },
-    ],
-    filterMode: 'tree',
-    filterSearch: true,
-    onFilter: (value, record) => record.name.includes(value),
-    width: '30%',
-  },
-
-  {
-    title: 'Activities',
-    dataIndex: 'url',
-    filters: [
-      {
-        text: 'samagra',
-        value: 'https://yaus.xyz/samagra',
-
-      },
-      {
-        text: 'first',
-        value: 'https://yaus.xyz/first',
-      },
-      {
-        text: 'pass',
-        value: 'https://yaus.xyz/pass',
-      },
-      {
-        text: 'hack',
-        value: 'https://yaus.xyz/hack',
-      },
-    ],
-    onFilter: (value, record) => record.url.startsWith(value),
-    filterSearch: true,
-    width: '40%',
-  },
-
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    filters: [
-      {
-        text: 'Kanika',
-        value: 'kanika',
-      },
-      {
-        text: 'Akshay',
-        value: 'akshay',
-
-      },
-      {
-        text: 'Chakshu',
-        value: 'chakshu',
-
-      },
-      {
-        text: 'Manav',
-        value: 'manav',
-
-      },
-    ],
-    filterMode: 'tree',
-    filterSearch: true,
-    onFilter: (value, record) => record.name.includes(value),
-    width: '30%',
-  },
-
-  {
-    title: 'Email',
-    dataIndex: 'email',
-    filters: [
-      {
-        text: 'kanikagola26@gmail.com',
-        value: 'kanikagola26@gmail.com',
-      },
-      {
-        text: 'akshay@gmail.com',
-        value: 'akshay@gmail.com',
-
-      },
-      {
-        text: 'chakshu@gmail.com',
-        value: 'chakshu@gmail.com',
-
-      },
-      {
-        text: 'manav@gmail.com',
-        value: 'manav@gmail.com',
-
-      },
-    ],
-    filterMode: 'tree',
-    filterSearch: true,
-    onFilter: (value, record) => record.name.includes(value),
-    width: '30%',
-  },
-
-
-];
+);
 
 async function recent() {
-  const url = 'http://localhost:3233/recent_activity_data'
+  const url = "http://localhost:3233/recent_activity_data";
   const response = await fetch(url);
 
   const recent_data = await response.json();
@@ -181,99 +56,53 @@ async function recent() {
 
 recent();
 
-recent().then(recent_data => {
-  const activity = recent_data.map(
-    function (index) {
+recent()
+  .then((recent_data) => {
+    const activity = recent_data.map(function (index) {
       return index.activity;
-    }
-  )
+    });
 
-  const name = recent_data.map(
-    function (index) {
+    const name = recent_data.map(function (index) {
       return index.name;
-    }
-  )
+    });
 
-  const email = recent_data.map(
-    function (index) {
+    const email = recent_data.map(function (index) {
       return index.email;
-    }
-  )
+    });
 
-  const title = recent_data.map(
-    function (index) {
+    const title = recent_data.map(function (index) {
       return index.title;
-    }
-  )
+    });
 
-  const date_created = recent_data.map(
-    function (index) {
+    const date_created = recent_data.map(function (index) {
       return index.date_created;
+    });
+    for (let i = 0; i < data.length; i++) {
+      data[i].date = date_created[i];
     }
-  )
-  for (let i = 0; i < data.length; i++) {
-    data[i].date = date_created[i];
-  }
 
-  for (let i = 0; i < data.length; i++) {
-    data[i].name = name[i];
-  }
+    for (let i = 0; i < data.length; i++) {
+      data[i].name = name[i];
+    }
 
-  for (let i = 0; i < data.length; i++) {
-    data[i].email = email[i];
-  }
-  for (let i = 0; i < data.length; i++) {
-    const url_data = activity[i];
-    data[i].url = <a href="{url_data}">{url_data}</a>;
-  }
+    for (let i = 0; i < data.length; i++) {
+      data[i].email = email[i];
+    }
+    for (let i = 0; i < data.length; i++) {
+      const url_data = activity[i];
+      data[i].url = <a href="{url_data}">{url_data}</a>;
+    }
 
-  for (let i = 0; i < data.length; i++) {
-    data[i].org = title[i];
-  }
-
-}).catch((error) => {
-  console.log('fetch data failed', error);
-})
-const data = [
-  {
-    date: "",
-    org: "",
-    url: <a href=""></a>,
-    name: "",
-    email: ""
-  },
-  {
-    date: "",
-    org: "",
-    url: <a href=""></a>,
-    name: "",
-    email: ""
-  },
-  {
-    date: "",
-    org: "",
-    url: <a href=""></a>,
-    name: "",
-    email: ""
-  },
-  {
-    date: "",
-    org: "",
-    url: <a href=""></a>,
-    name: "",
-    email: ""
-  },
-];
+    for (let i = 0; i < data.length; i++) {
+      data[i].org = title[i];
+    }
+  })
+  .catch((error) => {
+    console.log("fetch data failed", error);
+  });
 
 const onChange = (pagination, filters, sorter, extra) => {
-  console.log('params', pagination, filters, sorter, extra);
+  console.log("params", pagination, filters, sorter, extra);
 };
-
-
-
-
-
-
-
 
 export default App;
