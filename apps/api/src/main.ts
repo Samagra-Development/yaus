8be8d09cf49e625cbd11af17eabb39a8837dadbb
Rishabh Jain <jain.rishabhche@gmail.com>
2023-07-21 00:45:09 +0530

    Fix : Clicks_count published on RMQ

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,16 +3,16 @@
  * This is only a minimal backend to get started.
  */
 
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { MicroserviceOptions, Transport } from "@nestjs/microservices";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import {
   FastifyAdapter,
   NestFastifyApplication,
-} from '@nestjs/platform-fastify';
+} from "@nestjs/platform-fastify";
 
-import { AppModule } from './app/app.module';
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   // API for YAUS
@@ -20,35 +20,35 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter()
   );
-  app.enableCors()
+  app.enableCors();
 
-  // // MS for managing side-effects
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.RMQ,
-  //   options: {
-  //     urls: ['amqp://username:password@localhost:5672'],
-  //     queue: 'clicks',
-  //     queueOptions: {
-  //       durable: false,
-  //     },
-  //   },
-  // });
+  // MS for managing side-effects
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ["amqp://username:password@localhost:5672"],
+      queue: "clicks",
+      queueOptions: {
+        durable: false, // was false
+      },
+    },
+  });
 
-  const globalPrefix = process.env.APP_GLOBAL_PREFIX || '';
+  const globalPrefix = process.env.APP_GLOBAL_PREFIX || "";
   app.setGlobalPrefix(globalPrefix);
 
   const config = new DocumentBuilder()
-    .setTitle('YAUS - Yet Another URL Shortener')
-    .setDescription('YAUS APIS')
-    .setVersion('1.0')
-    .addTag('yaus')
+    .setTitle("YAUS - Yet Another URL Shortener")
+    .setDescription("YAUS APIS")
+    .setVersion("1.0")
+    .addTag("yaus")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup("api", app, document);
 
   const port = process.env.PORT || 3333;
   await app.startAllMicroservices();
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port, "0.0.0.0");
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
