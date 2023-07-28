4e9fc9009d1ab76f5325eb5b8710ac6311cc77c4
Rishabh Jain <jain.rishabhche@gmail.com>
2023-07-29 02:45:06 +0530

    Feat(Link-expiry): Added link expiry using redis

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -4,6 +4,7 @@ import { PrismaService } from './prisma.service';
 import { link, Prisma } from '@prisma/client';
 import { ConfigService } from '@nestjs/config'
 import { TelemetryService } from './telemetry/telemetry.service';
+import { Link } from './app.interface';
 
 @Injectable()
 export class AppService {
@@ -14,9 +15,13 @@ export class AppService {
     private telemetryService: TelemetryService,
     ) {}
 
-  async setKey(hashid: string): Promise<void> {
+  async setKey(link:Link,Data:Prisma.linkCreateInput): Promise<void> {
     const client = await this.redisService.getClient(this.configService.get<string>('REDIS_NAME'));
-    client.set(hashid, 0);
+    // get expiration time from params field in link
+    let ttl = parseInt(Data.params["expiry"]);  // time to live in seconds
+    console.log("The link expiry is set to:"+ ttl);
+    client.set(link.hashid.toString(), JSON.stringify(link));
+    !Number.isNaN(ttl) ? client.expire(link.hashid.toString(), ttl) : 0;
   }
   
   async updateClicks(urlId: string): Promise<void> {
@@ -29,7 +34,11 @@ export class AppService {
     const keys: string[] = await client.keys('*');
     return keys
   }
-
+  async fetchAKey(key: string): Promise<string> {
+    const client = await this.redisService.getClient(this.configService.get<string>('REDIS_NAME'));
+    const value: string = (await client.get(key)).toString();
+    return value
+  }
   async updateClicksInDb(): Promise<void> {
     const client = await this.redisService.getClient(this.configService.get<string>('REDIS_NAME'));
     const keys: string[] = await this.fetchAllKeys()
@@ -83,7 +92,7 @@ export class AppService {
         data,
       });
 
-      this.setKey(link.hashid.toString());
+      this.setKey(link,data);
       return link;
     }
 
@@ -105,29 +114,14 @@ export class AppService {
     }
 
     async redirect(hashid: string): Promise<string> {
-        return this.prisma.link.findMany({
-          where: {
-            OR: [
-              {
-                hashid: Number.isNaN(Number(hashid))? -1:parseInt(hashid),
-              },
-              { customHashId: hashid },
-            ],
-          },
-          select: {
-            url: true,
-            params: true,
-            hashid: true,
-          },
-          take: 1
-        })
-        .then(response => {
-          const url = response[0].url
-          const params = response[0].params
-          const ret = [];
-          
-          this.updateClicks(response[0].hashid.toString());
 
+          return this.fetchAKey(hashid).then((value: string) => {
+          const link = JSON.parse(value);
+          console.log("The link is:"+ link.url);
+          const url = link.url
+          const params = link.params
+          const ret = [];
+          console.log("The params are:"+ params + url);
           if(params == null){
             return url;
           }else {
@@ -138,8 +132,44 @@ export class AppService {
           }
         })
         .catch(err => {
-          this.telemetryService.sendEvent(this.configService.get<string>('POSTHOG_DISTINCT_KEY'), "Exception in getLinkFromHashIdOrCustomHashId query", {error: err.message})
-          return '';
-        });
+            this.telemetryService.sendEvent(this.configService.get<string>('POSTHOG_DISTINCT_KEY'), "Exception in getLinkFromHashIdOrCustomHashId query", {error: err.message})
+            return '';
+          });
+        // return this.prisma.link.findMany({
+        //   where: {
+        //     OR: [
+        //       {
+        //         hashid: Number.isNaN(Number(hashid))? -1:parseInt(hashid),
+        //       },
+        //       { customHashId: hashid },
+        //     ],
+        //   },
+        //   select: {
+        //     url: true,
+        //     params: true,
+        //     hashid: true,
+        //   },
+        //   take: 1
+        // })
+        // .then(response => {
+        //   const url = response[0].url
+        //   const params = response[0].params
+        //   const ret = [];
+          
+        //   // this.updateClicks(response[0].hashid.toString());
+
+        //   if(params == null){
+        //     return url;
+        //   }else {
+        //     Object.keys(params).forEach(function(d) {
+        //       ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(params[d]));
+        //     })
+        //     return `${url}?${ret.join('&')}` || '';
+        //   }
+        // })
+        // .catch(err => {
+        //   this.telemetryService.sendEvent(this.configService.get<string>('POSTHOG_DISTINCT_KEY'), "Exception in getLinkFromHashIdOrCustomHashId query", {error: err.message})
+        //   return '';
+        // });
       }
 }
